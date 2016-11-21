fed3c21466f3f747a9d57b468d13d6bd8732147f
omokehinde olamide <omokehinde olamide>
2016-11-21 10:10:58 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,37 @@
+class App<Sinatra::Base
+    get '/' do
+        # @auth = authorized?
+        @images = Image.all
+        haml :index
+    end
+
+    post '/images'do
+        protected!
+        @image = Image.new params[:image]
+        @image.save
+        redirect "/"
+    end
+
+    get '/images/:id' do
+        @image = Image[params[:id]]
+        haml :show
+    end
+
+    get '/auth' do
+        protected!
+        redirect '/'
+    end
+
+    helpers do
+        def protected!
+            return if authorized?
+            headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+            halt 401, "Not authorized\n"
+        end
+        
+        def authorized?
+            @auth ||=Rack::Auth::Basic::Request.new(request.env)
+            @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin'] 
+        end
+    end
+end
